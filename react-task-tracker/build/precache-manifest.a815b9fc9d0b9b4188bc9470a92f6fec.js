a0d0d65d5001b84d80bfcc7bde196fbf54d15d84
TheSunnyDev <109054640+TheSunnyDev@users.noreply.github.com>
2022-11-12 16:53:47 -0800

    Add React Task App files

---
+++ react-task-tracker/build/precache-manifest.a815b9fc9d0b9b4188bc9470a92f6fec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08270543bb48d9863072",
+    "url": "/static/css/main.e8b65147.chunk.css"
+  },
+  {
+    "revision": "08270543bb48d9863072",
+    "url": "/static/js/main.08270543.chunk.js"
+  },
+  {
+    "revision": "69e0dab03ec9f6c99eb0",
+    "url": "/static/js/1.69e0dab0.chunk.js"
+  },
+  {
+    "revision": "44faef076067e302bbdd",
+    "url": "/static/js/2.44faef07.chunk.js"
+  },
+  {
+    "revision": "56939e6b754efe784d27",
+    "url": "/static/js/runtime~main.56939e6b.js"
+  },
+  {
+    "revision": "14ccbacd5b5c61f43e2280bc256beab1",
+    "url": "/index.html"
+  }
+];
